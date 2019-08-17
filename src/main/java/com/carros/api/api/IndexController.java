2764ed7d6055604495b093656b2a12b4695c9800
Allef sousa <allefsousa_1@hotmail.com>
2019-08-17 20:49:14 -0300

    Metodos Http

--- src/main/java/com/carros/api/api/IndexController.java
+++ src/main/java/com/carros/api/api/IndexController.java
@@ -1,4 +1,27 @@
 package com.carros.api.api;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
 public class IndexController {
+
+    @GetMapping
+    public String get(){
+        return "hello";
+    }
+    @PostMapping
+    public String post(){
+        return "hello post";
+    }
+    @PutMapping
+    public String put(){
+        return "hello put";
+    }
+    @DeleteMapping
+    public String delete(){
+        return "hello delete";
+    }
+
 }
+
