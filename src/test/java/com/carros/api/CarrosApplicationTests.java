78a14f15f4b35c8d26c8d97724c9ed311b4f6b2c
Allef Sousa <allefsousa_1@hotmail.com>
2019-09-15 16:22:31 -0300

    Criando test para inserir e remover registros

--- src/test/java/com/carros/api/CarrosApplicationTests.java
+++ src/test/java/com/carros/api/CarrosApplicationTests.java
@@ -1,13 +1,49 @@
 package com.carros.api;
 
+import com.carros.api.domain.Carro;
+import com.carros.api.domain.CarroService;
+import com.carros.api.domain.dto.CarroDTO;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CarrosApplicationTests {
+	@Autowired
+	private CarroService service;
+
+	@Test
+	public void testSaveNewCar(){
+		Carro carro = new Carro();
+		carro.setTipo("esportivo");
+		carro.setNome("Ferrari");
+
+		//salvando carro
+		CarroDTO carroDto = service.saveCarro(carro);
+		assertNotNull(carroDto);
+
+		//buscando carro pelo id
+		Long id = carro.getId();
+		assertNotNull(id);
+		Optional<CarroDTO> op = service.getCarrosById(id);
+		assertTrue(op.isPresent());
+
+		carroDto = op.get();
+		assertEquals(carro.getNome(),carroDto.getNome());
+		assertEquals(carro.getTipo(),carroDto.getTipo());
+
+		// deletando carro
+		service.deleteCarro(id);
+		assertFalse(service.getCarrosById(id).isPresent());
+
+	}
 
 	@Test
 	public void contextLoads() {
